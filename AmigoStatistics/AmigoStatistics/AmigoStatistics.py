5d11acca4ae994ad5d7139cae793af6d13261dc2
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-02-02 10:28:22 -0500

    moved populate_dict_with_hierarchy to logic folder

--- AmigoStatistics/AmigoStatistics/AmigoStatistics.py
+++ AmigoStatistics/AmigoStatistics/AmigoStatistics.py
@@ -199,84 +199,6 @@ class AmigoStatisticsWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
 
     self._parameterNode.EndModify(wasModified)
 
-  def populate_dict_with_hierarchy(self, sh_folder_item_id, patient_id, storage_dict, scene_path, hierarchy_ori=None):
-    """
-    Populate a dict entry with the hierarchy of an opened scene
-    """
-
-    hierarchy = hierarchy_ori
-
-    if not hierarchy_ori:
-      hierarchy_ori = ""
-
-    if not hierarchy:
-      hierarchy = ""
-    else:
-      hierarchy = hierarchy.split('/')
-      del hierarchy[0]
-
-      if any(x in hierarchy[0].lower() for x in ["patient", patient_id]):
-        del hierarchy[0]  # remove first element because it's the patient case
-
-    child_ids = vtk.vtkIdList()
-    sh_node = slicer.vtkMRMLSubjectHierarchyNode.GetSubjectHierarchyNode(slicer.mrmlScene)
-    sh_node.GetItemChildren(sh_folder_item_id, child_ids)
-    if child_ids.GetNumberOfIds() == 0:
-      return
-
-    # Write each child item to file
-    for itemIdIndex in range(child_ids.GetNumberOfIds()):
-      sh_item_id = child_ids.GetId(itemIdIndex)
-      # Write node to file (if storable)
-      data_node = sh_node.GetItemDataNode(sh_item_id)
-      if data_node and data_node.IsA("vtkMRMLStorableNode") and data_node.GetStorageNode():
-        storage_node = data_node.GetStorageNode()
-        filename = os.path.basename(storage_node.GetFileName())
-
-        if patient_id not in storage_dict:
-          storage_dict = json_dict_logic.create_empty_dict_entry(storage_dict, patient_id, scene_path)
-
-        if not hierarchy:  # we are at the end
-          return storage_dict
-
-        if all(x in hierarchy[0].lower() for x in ["pre", "op", "imaging"]):
-          storage_dict[patient_id]["pre-op imaging"].append(filename)
-
-        elif all(x in hierarchy[0].lower() for x in ["intra", "op", "imaging"]):
-          if len(hierarchy) == 1:
-            storage_dict[patient_id]["intra-op imaging"]["rest"].append(filename)
-          else:
-            storage_dict[patient_id]["intra-op imaging"]["ultrasounds"].append(filename)
-
-        elif all(x in hierarchy[0].lower() for x in ["contin", "tracking"]):
-          if len(hierarchy) == 1:
-            if "post" in filename.lower():
-              storage_dict[patient_id]["continuous tracking data"]["post-imri tracking"].append(filename)
-            else:
-              storage_dict[patient_id]["continuous tracking data"]["pre-imri tracking"].append(filename)
-
-          elif all(x in hierarchy[1].lower() for x in ["pre", "imri", "tracking"]):
-            storage_dict[patient_id]["continuous tracking data"]["pre-imri tracking"].append(filename)
-          elif all(x in hierarchy[1].lower() for x in ["post", "imri", "tracking"]):
-            storage_dict[patient_id]["continuous tracking data"]["post-imri tracking"].append(filename)
-
-        elif "segmentations" in hierarchy[0].lower():
-          if len(hierarchy) == 1:
-            storage_dict[patient_id]["segmentations"]["rest"].append(filename)
-          elif all(x in hierarchy[1].lower() for x in ["pre-op", "fmri"]):
-            storage_dict[patient_id]["segmentations"]["pre-op fmri segmentations"].append(filename)
-          elif all(x in hierarchy[1].lower() for x in ["brainlab", "dti"]):
-            storage_dict[patient_id]["segmentations"]["pre-op brainlab manual dti tractography segmentations"].append(filename)
-
-      # Write all children of this child item
-      grand_child_ids = vtk.vtkIdList()
-      sh_node.GetItemChildren(sh_item_id, grand_child_ids)
-      if grand_child_ids.GetNumberOfIds() > 0:
-        self.populate_dict_with_hierarchy(sh_item_id, patient_id, storage_dict, scene_path,
-                                     hierarchy_ori + "/" + sh_node.GetItemName(sh_item_id))
-
-    return storage_dict
-
   def dump_hierarchy_to_json(self, patient_id, data_json_path, scene_path):
     """
     Dumps entire hierarchy to a json
@@ -301,7 +223,7 @@ class AmigoStatisticsWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
     slicer.app.ioManager().addDefaultStorageNodes()
 
     if str(patient_id) not in patients_dict:  # it was already parsed at some point
-      patients_dict = self.populate_dict_with_hierarchy(shNode.GetSceneItemID(), patient_id, patients_dict, scene_path)
+      patients_dict = json_dict_logic.populate_dict_with_hierarchy(shNode.GetSceneItemID(), patient_id, patients_dict, scene_path)
 
       f = open(data_json_path, "a")
       json.dump(patients_dict, f)
