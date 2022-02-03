8ffe8c69dbf4f1682b73b6c4164fb37f429a6319
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-02-02 20:58:35 -0500

    small script to extract all .mrb files from a given folder

---
+++ AmigoStatistics/AmigoStatistics/extract_mrn_paths.py
@@ -0,0 +1,13 @@
+import os
+
+root_directory = "/Users/fryderykkogl/Dropbox (Partners HealthCare)/TCIA/TCIA cases ALL"
+
+x = []
+
+for path, subdirs, files in os.walk(root_directory):
+    for name in files:
+        if ".mrb" in name:
+            print(os.path.join(path, name))
+            x.append(os.path.join(path, name))
+
+print(len(x))
