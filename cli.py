4bb743a60075715a81441e9e90b68c83e9825e89
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-03-08 10:27:00 -0500

    removed command line arguments - paramteters are passed through the gUI now

--- cli.py
+++ cli.py
@@ -1,12 +1,4 @@
 from deidentify_mp4.main_func import main
-import argparse
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("-lp", "--load_path", type=str, required=True,
-                        help="Path to the .dicom file to be de-identified")
-
-    arguments = parser.parse_args()
-
-    main(arguments)
+    main()
