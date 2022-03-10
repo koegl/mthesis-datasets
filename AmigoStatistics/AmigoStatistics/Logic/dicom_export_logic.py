6b04d046b93bfbeb74f85d20b781815528826fdd
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-03-10 16:37:03 -0500

    function to save landmarks to fcsv

--- AmigoStatistics/AmigoStatistics/Logic/dicom_export_logic.py
+++ AmigoStatistics/AmigoStatistics/Logic/dicom_export_logic.py
@@ -362,6 +362,37 @@ class DicomExportLogic:
                 self.logger.log(logging.ERROR, f"Could not export node {node.name}.\n({str(e)})\n"
                                                f"export_segmentations_to_dicom")
 
+    def export_landmarks_to_fcsv(self):
+        """
+        Export landmark to fcsv
+        """
+        bfs_array = Tree.bfs(self.folder_structure)
+
+        counter = {}
+        for node in bfs_array:
+            try:
+                if not bool(node.children) \
+                        and "transform" not in node.name.lower() \
+                        and "segment" not in node.parent.name.lower() \
+                        and "landmark" in node.parent.name.lower():
+
+                    # increase/create counter for series number
+                    if node.parent.name in counter:
+                        counter[node.parent.name] += 1
+                    else:
+                        counter[node.parent.name] = 1
+
+                    markups_node = slicer.mrmlScene.GetFirstNode(node.name)
+
+                    if markups_node is None:
+                        raise ValueError(f"Could not find and export landmarks node {node.name}")
+
+                    slicer.util.saveNode(markups_node,
+                                         os.path.join(self.output_folder, "landmarks_" + self.study_instance_uid))
+            except Exception as e:
+                self.logger.log(logging.ERROR, f"Could not export node {node.name}.\n({str(e)})\n"
+                                               f"export_landmarks_to_fcsv")
+
     def full_export(self):
         """
         Perform all the steps to export
