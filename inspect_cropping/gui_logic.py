0e705c82791fc6887e6a18ae42edb093c8e20dc7
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-04-07 20:50:24 -0400

    added basic file struture for file inspection

---
+++ inspect_cropping/gui_logic.py
@@ -0,0 +1,8 @@
+import dearpygui.dearpygui as dpg
+
+from utils import extract_file_paths_with_extension
+
+
+class GUIWindow:
+    def __init__(self):
+        pass
---
+++ inspect_cropping_start.py
@@ -0,0 +1,4 @@
+from inspect_cropping.main_func import main
+
+if __name__ == "__main__":
+    main()
