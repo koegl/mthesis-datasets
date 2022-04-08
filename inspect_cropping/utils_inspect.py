3c2708446ac54c31ffe4b428f96a82f018e9fd6c
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-04-08 11:55:38 -0400

    addef ile extraction function to inspect utils

--- inspect_cropping/utils_inspect.py
+++ inspect_cropping/utils_inspect.py
@@ -27,3 +27,41 @@ def load_mp4(path):
     frames_np = np.array(frames)
 
     return frames_np
+
+
+def extract_file_paths_with_extension(parent_folder_path, extension="", exclude=None):
+    """
+    Extract all file paths from the folder, sub-folders etc. of the given extension. Empty extension ("") means .dcm
+    :param parent_folder_path: Path to the parent folder
+    :param extension: Extension of the files to be extracted
+    :param exclude: keyword in path that would exclude the file from the list
+    :return: list of all file paths
+    """
+
+    # get all files
+    all_files_buf = []
+    for root, _, files in os.walk(parent_folder_path):
+        for file in files:
+            buf_path = os.path.join(root, file)
+
+            # check for correct extension
+            buf_file_name = os.path.basename(buf_path)
+            file_name_split = buf_file_name.split(".")
+
+            if len(file_name_split) == 1:  # it's a dicom without an extension
+                current_extension = ""
+            else:
+                current_extension = file_name_split[1]
+
+            if "ds_store" not in buf_path.lower() and "dicomdir" not in buf_path.lower() and \
+                    current_extension == extension:
+
+                if exclude:
+                    if exclude not in buf_path.lower():
+                        all_files_buf.append(buf_path)
+                else:
+                    all_files_buf.append(buf_path)
+
+    all_files_buf.sort()
+
+    return all_files_buf
