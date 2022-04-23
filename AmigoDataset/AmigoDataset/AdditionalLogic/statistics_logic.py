80f8fb8af6a3d0de514b1c1f05092adfc2ddde9b
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-04-23 17:40:34 -0400

    initial statistics class

---
+++ AmigoDataset/AmigoDataset/AdditionalLogic/statistics_logic.py
@@ -0,0 +1,12 @@
+
+from AdditionalLogic.tree import Tree
+
+
+class StatisticsExporingLogic:
+    """
+    Class to encapsulate logic for exporting mrb volume statistics.
+    """
+
+    def __init__(self):
+        pass
+
