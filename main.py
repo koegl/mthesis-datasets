85e41fc96e294b87dfbad9821f4c47f8854475ff
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-05-05 19:21:37 -0400

    updated paths in main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import argparse
 from time import perf_counter
 
-from utils import extract_features, match_features
+from utils import extract_features, match_features, write_features_to_fcsv
 
 
 def main(args):
@@ -13,18 +13,22 @@ def main(args):
 
     # extract features
     start_time = perf_counter()
-    file1_features_path = extract_features(file1_path)
-    file2_features_path = extract_features(file2_path)
+    # file1_features_path = extract_features(file1_path)
+    # file2_features_path = extract_features(file2_path)
     end_time = perf_counter()
     print(f"\nFeature extraction time: {end_time - start_time}\n")
 
     # feature matching
-
     start_time = perf_counter()
-    match_features(file1_features_path, file2_features_path)
+    file1_features_path = "/Users/fryderykkogl/Data/nifti_test/785522447/Intra-op US/US1_features.key"
+    file2_features_path = "/Users/fryderykkogl/Data/nifti_test/785522447/Intra-op US/US2_features.key"
+    features = match_features(file1_features_path, file2_features_path)
     end_time = perf_counter()
     print(f"\nFeature matching time: {end_time - start_time}\n")
 
+    # convert features to .fcsv
+    write_features_to_fcsv(features[0], file1_path.split(".")[0] + ".fcsv")
+    write_features_to_fcsv(features[1], file2_path.split(".")[0] + ".fcsv")
 
 
 if __name__ == '__main__':
